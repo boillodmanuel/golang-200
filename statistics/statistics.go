8cce174f8cdf0672e08db15d90b0d74bb79f6afa
Manuel Boillod <boillodmanuel@gmail.com>
2018-11-27 14:30:16 +0100

    tp

--- statistics/statistics.go
+++ statistics/statistics.go
@@ -1,6 +1,8 @@
 package statistics
 
 import (
+	"fmt"
+	logger "github.com/sirupsen/logrus"
 	"time"
 )
 
@@ -11,6 +13,10 @@ const (
 // TODO define a Statistics struct with an uint8 chan, an uint32 counter, a start time and logging period duration
 // Statistics is the worker to persist the request statistics
 type Statistics struct {
+	ch                    chan uint8
+	counter               uint32
+	startTime             time.Time
+	loggingPeriodDuration time.Duration
 }
 
 // NewStatistics creates a new statistics structure and launches its worker routine
@@ -18,22 +24,47 @@ func NewStatistics(loggingPeriod time.Duration) *Statistics {
 	// TODO build a new Statistics object with a sized channel, initialized counter and start time
 	// TODO and logging period as param
 
+	ch := make(chan uint8, statisticsChannelSize)
+
+	stats := &Statistics{
+		ch,
+		0,
+		time.Now(),
+		loggingPeriod,
+	}
 	// TODO launch the run in a separate Go routine in background
+	go stats.run()
 
 	// TODO return the initialized and started object
-	return nil
+	return stats
 }
 
 // PlusOne is used to send a statistics hit increment
 func (sw *Statistics) PlusOne() {
 	// TODO push a hit in the statistics channel
+	select {
+	case sw.ch <- 1:
+	default:
+		logger.Warn("message PlusOne not sent")
+	}
 }
 
 func (sw *Statistics) run() {
 	// TODO build a new time Ticker from the logging period
+	ticker := time.Tick(sw.loggingPeriodDuration)
 
 	// TODO build a infinite loop and the channel selection inside
-
+	for {
+		select {
+		case msg := <-sw.ch:
+			sw.counter += uint32(msg)
+		case tick := <-ticker:
+			tick.Clock()
+			rate := float64(sw.counter) / sw.loggingPeriodDuration.Seconds()
+			logger.Info("apps has received ", fmt.Sprintf("%.2f", rate), " per second (period: ", sw.loggingPeriodDuration, ")")
+			sw.counter = 0
+		}
+	}
 	// TODO build a first select case from the statistics channel
 	// TODO add the hit count to the counter and log it as debug level
 
--- statistics/statistics_test.go
+++ statistics/statistics_test.go
@@ -11,9 +11,24 @@ const (
 
 func TestStatistics(t *testing.T) {
 
-	//statistics := NewStatistics(2 * statPeriod)
+	statistics := NewStatistics(statPeriod)
 
 	//TODO: implement this test to test the statistics middleware (test the value of its counter)
 	//TODO: you will need time.Sleep(statPeriod) because of the statistics ticker
 
+	go func() {
+		statistics.PlusOne()
+	}()
+
+	time.Sleep(statPeriod / 2)
+
+	if statistics.counter != 1 {
+		t.Error("Statistics counter should be 1. Actual value is ", statistics.counter)
+	}
+
+	time.Sleep(statPeriod)
+
+	if statistics.counter != 0 {
+		t.Error("Statistics counter should be 0. Actual value is ", statistics.counter)
+	}
 }
